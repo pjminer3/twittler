0a380f987a14d928248e171ebe753dd26d31b752
Patrick Miner <patrickminer318@gmail.com>
2017-09-24 21:45:46 -0700

    Created HTML structure and refactored tweet generation into own function; createNewTweet

--- twittler.js
+++ twittler.js
@@ -3,32 +3,53 @@ $(document).ready(function(){
   var $body = $('body');
   //$body.html('');
 
+
+  var createNewTweet = function() {
+    var tweet = streams.home[index];
+    // CREATES NEW TWEET
+    var $tweet = $('<div class = "tweet"><div class = "profile"><img src="ADD IMAGE HERE"><h5> <!-- User name --/> </h5></div><div class="tweetContent"><p> <!-- body of tweet -/-> </p></div></div>');
+
+      // ADDS USERS NAME TO  RESPECTIVE ELEMENT
+      $tweet.text('@' + tweet.user + ': ' + tweet.message + '---' + tweet.created_at);
+      
+      // Add Tweet Content to proper element
+
+
+      // Add user image to proper element
+
+
+      $tweet.prependTo($('.tweetList'));  // ADD PROPER ELEMENT TO PREPEND IT 
+  }
+
+
+
+
   // Displays all starter tweets
   var endIndex = streams.home.length - 1;
   var index = streams.home.length - 1;
   while(index >= 0){
-    var tweet = streams.home[index];
-    var $tweet = $('<div></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message + '---' + tweet.created_at);
-    $tweet.prependTo($body);
+    createNewTweet();
     index -= 1;
   }
+
+
+  
 
   // Grabs new tweets that haven't been displayed yet and displays them in order, most recent at the top
   let displayNewTweets = function() {
     endIndex = streams.home.length - 1;
     while (index < endIndex) {
       index += 1;
-      var tweet = streams.home[index];
-      var $tweet = $('<div></div>');
-      $tweet.text('@' + tweet.user + ': ' + tweet.message + '---' + tweet.created_at);
-      $tweet.prependTo($body);
+      createNewTweet();
     }
   };
 
   // Calls displayNewTweets every second to keep the feed fresh
   setInterval(displayNewTweets, 1000);
 
+
+
+  
 
 
 });
